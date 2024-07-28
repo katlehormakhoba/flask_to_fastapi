6f0f0de8d80db149779d4c0426185417565c439b
Katleho R Makhoba <katlehormakhoba@gmail.com>
2024-07-28 12:48:39 +0200

    added career

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route("/career")
+def main():
+    return "Aspiring full stack developer"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
